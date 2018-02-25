d1e5301fb4c6302afc99f4d9dbf7ce872aae86ae
Geoff Day <geoffday67@gmail.com>
2018-02-25 21:17:55 +0000

    Initial commit

---
+++ app/src/main/java/uk/co/sullenart/kallingkard/BaseActivity.kt
@@ -0,0 +1,17 @@
+package uk.co.sullenart.kallingkard
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.annotation.LayoutRes
+import android.support.v7.app.AppCompatActivity
+import butterknife.ButterKnife
+
+@SuppressLint("Registered")
+open class BaseActivity(@LayoutRes private val content: Int) : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(content)
+        ButterKnife.bind(this)
+        //(application as MainApplication).component.inject(this)
+    }
+}
---
+++ app/src/main/java/uk/co/sullenart/kallingkard/MainActivity.kt
@@ -0,0 +1,42 @@
+package uk.co.sullenart.kallingkard
+
+import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import butterknife.BindView
+
+class MainActivity : BaseActivity(R.layout.activity_main) {
+
+    //fragments: CallingFragment[] = {}
+
+    @BindView(R.id.toolbar)
+    lateinit var toolbar: Toolbar
+
+    @BindView(R.id.drawer_layout)
+    lateinit var drawerLayout: DrawerLayout
+
+    /*@BindView()
+    lateinit var drawerLayout: DrawerLayout*/
+
+    //@SuppressLint("RestrictedApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean =
+            when (item?.itemId) {
+                android.R.id.home -> {
+                    drawerLayout.openDrawer(GravityCompat.START)
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+}
